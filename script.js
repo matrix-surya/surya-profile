b15fe37f927529942b58675246d4d61f155e54f0
matrix-surya <ruthalasurya1413@gmail.com>
2024-07-11 11:01:23 +0530

    done

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Programmer . . ." , "Web Developer . . ." , "Gen AI developer . . ."],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
